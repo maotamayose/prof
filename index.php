081bd2ccb212e716b5c369a2257e85cc7a3dad49
maotamayose <mao03051@outlook.jp>
2025-02-17 21:43:36 +0900

    Seccond commit

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>プロフィール</title>
-    <link rel="stylesheet" href="style.css"> <!-- 外部CSSファイルを読み込む -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <!-- ヒーローセクション -->
 <section class="hero">
-    <video autoplay loop muted playsinline>
+    <video class="hero-video" autoplay loop muted playsinline>
         <source src="video/hero-video.mp4" type="video/mp4">
     </video>
     <div class="hero-text">
         <h1>ようこそ！</h1>
-        <p>玉代勢真生のプロフィールページへ</p>
+        <p>玉代勢のプロフィールページへ</p>
     </div>
 </section>
 
@@ -35,22 +35,22 @@
             <li>ゲーム（Valorant）</li>
             <li>漫画（ワンピース、亜人、左利きのエレン）</li>
             <li>読書（湊かなえ）</li>
-            <li>音楽鑑賞（PEOPLE1　PUNPEE）</li>
+            <li>音楽鑑賞（PEOPLE1 PUNPEE）</li>
         </ul>
     </div>
 </section>
 
 <!-- 動画＋テキストのブロック（性格） -->
-<section class="content-block reverse">
-    <div class="text-container">
-        <h2>性格</h2>
-        <p>出来るまで、<br>試行錯誤を重ねて、<br>前向きに取り組むこと。<br>僕のモットーです。</p>
-        <p>初メイクはへたくそでしたが、、<br>努力して上の動画のように！！</p>
-    </div>
+<section class="content-block">
     <div class="video-container">
         <video autoplay loop muted playsinline>
             <source src="video/video2.mp4" type="video/mp4">
         </video>
+    </div>
+    <div class="text-container">
+        <h2>性格</h2>
+        <p>出来るまで、<br>試行錯誤を重ねて、<br>前向きに取り組むこと。<br>僕のモットーです。</p>
+        <p>初メイクはへたくそでしたが、、<br>努力して上の動画のように！！</p>
     </div>
 </section>
 </body>
